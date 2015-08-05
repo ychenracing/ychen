3086716156da1995545fa1214bcffe31f9add785
ychenracing <ychenracing@gmail.com>
2015-08-05 23:45:27 +0800

    create

---
+++ src/cn/edu/fudan/iipl/dao/ArticleDao.java
@@ -0,0 +1,59 @@
+/**
+ * iipl.fudan.edu.cn
+ * Copyright (c)
+ */
+package cn.edu.fudan.iipl.dao;
+
+import java.sql.Date;
+import java.util.List;
+
+import cn.edu.fudan.iipl.entity.Article;
+
+/**
+ * article dao
+ * @author racing
+ * @version $Id: ArticleDao.java, v 0.1 Aug 5, 2015 11:35:51 PM racing Exp $
+ */
+public interface ArticleDao {
+
+    /**
+     * 查询所有文章
+     * @return List<Article>
+     */
+    public List<Article> findAll();
+
+    /**
+     * 根据id查找文章
+     * @param id
+     * @return Article
+     */
+    public Article getById(int id);
+
+    /**
+     * 根据标题查找文章
+     * @param title
+     * @return Article
+     */
+    public Article getByTitle(String title);
+
+    /**
+     * 根据类别查找文章
+     * @param category
+     * @return List<Article>
+     */
+    public List<Article> getByCategory(String category);
+
+    /**
+     * 根据作者查找文章
+     * @param category
+     * @return List<Article>
+     */
+    public List<Article> getByAuthor(String category);
+
+    /**
+     * 根据创建时间查找文章
+     * @param gmtCreate
+     * @return List<Article>
+     */
+    public List<Article> getByGmtCreate(Date gmtCreate);
+}
