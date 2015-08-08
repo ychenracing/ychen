fb96c8978871057fdbc782b407bc45b271c3f7c5
ychenracing <ychenracing@gmail.com>
2015-08-08 18:53:11 +0800

    create convertor

---
+++ src/cn/edu/fudan/iipl/util/ArticleConvertor.java
@@ -0,0 +1,32 @@
+/**
+ * cn.edu.fudan.iipl
+ * Copyright (c).
+ */
+package cn.edu.fudan.iipl.util;
+
+import cn.edu.fudan.iipl.entity.Article;
+import cn.edu.fudan.iipl.form.ArticleForm;
+
+/**
+ * article转换类
+ * @author racing
+ * @version $Id: ArticleConvertor.java, v 0.1 Aug 8, 2015 6:49:01 PM racing Exp $
+ */
+public class ArticleConvertor {
+
+    /**
+     * ArticleForm转换成Article
+     * @param form
+     * @return Article
+     */
+    public static Article convertToArticleForm(ArticleForm form) {
+        Article article = new Article();
+        article.setTitle(form.getTitle());
+        article.setMainBody(form.getMainBody());
+        article.setCategory(form.getCategory());
+        article.setGmtCreate(form.getGmtCreate());
+        article.setGmtModify(form.getGmtModify());
+        article.setAuthor(form.getAuthor());
+        return article;
+    }
+}
