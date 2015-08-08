72ab34c92f9fab46eeba79f30db49d79407cd329
ychenracing <ychenracing@gmail.com>
2015-08-08 20:09:05 +0800

    add addArticle

--- src/cn/edu/fudan/iipl/web/ArticleController.java
+++ src/cn/edu/fudan/iipl/web/ArticleController.java
@@ -4,12 +4,14 @@
  */
 package cn.edu.fudan.iipl.web;
 
+import java.sql.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +20,8 @@ import cn.edu.fudan.iipl.dao.ArticleDao;
 import cn.edu.fudan.iipl.entity.Article;
 import cn.edu.fudan.iipl.enums.BlogExceptionEnum;
 import cn.edu.fudan.iipl.exception.BlogException;
+import cn.edu.fudan.iipl.form.ArticleForm;
+import cn.edu.fudan.iipl.util.ArticleConvertor;
 
 /**
  * 文章controller
@@ -33,15 +37,21 @@ public class ArticleController {
     private ArticleDao          articleDao;
 
     /** 文章列表页面 */
-    private static final String ARTICLE_LIST_JSP    = "/WEB-INF/jsp/article/article_list.jsp";
+    private static final String ARTICLE_LIST_JSP       = "/WEB-INF/jsp/article/article_list.jsp";
 
     /** 文章详情页面 */
-    private static final String ARTICLE_PROFILE_JSP = "/WEB-INF/jsp/article/article_profile.jsp";
+    private static final String ARTICLE_PROFILE_JSP    = "/WEB-INF/jsp/article/article_profile.jsp";
+
+    /** 创建文章页面 */
+    private static final String ADD_ARTICLE_JSP        = "/WEB-INF/jsp/article/add_article.jsp";
+
+    /** 文章创建结果页面 */
+    private static final String ADD_ARTICLE_RESULT_JSP = "/WEB-INF/jsp/article/add_article_result.jsp";
 
     /**
      * 显示文章列表页面
      * @param request
-     * @return
+     * @return 文章列表页面JSP
      */
     @RequestMapping(value = "/list", method = { RequestMethod.GET })
     public String showList(ModelMap model) {
@@ -53,7 +63,7 @@ public class ArticleController {
     /**
      * 显示文章内容页面
      * @param model
-     * @return
+     * @return 文章内容页面JSP
      */
     @RequestMapping(value = "/profile/{id}", method = { RequestMethod.GET })
     public String showProfile(ModelMap model, @PathVariable String id) {
@@ -67,5 +77,39 @@ public class ArticleController {
         Article article = articleDao.getById(articleId);
         model.addAttribute("article", article);
         return ARTICLE_PROFILE_JSP;
+    }
+
+    /**
+     * 显示创建文章页面
+     * @param model
+     * @return 文章创建页面JSP
+     */
+    @RequestMapping(value = "/addArticle", method = { RequestMethod.GET })
+    public String showAddArticle(ModelMap model) {
+        return ADD_ARTICLE_JSP;
+    }
+
+    /**
+     * 创建文章
+     * @param articleForm
+     * @param bindingResult
+     * @param model
+     * @return 文章创建结果页面JSP
+     */
+    @RequestMapping(value = "addArticle", method = { RequestMethod.POST })
+    public String doAddArticle(ArticleForm articleForm, BindingResult bindingResult, ModelMap model) {
+        if (bindingResult.hasErrors()) {
+            model
+                .addAttribute("errorMsg", bindingResult.getFieldErrors().get(0).getRejectedValue());
+            return ADD_ARTICLE_JSP;
+        }
+        Date date = new Date(System.currentTimeMillis());
+        articleForm.setGmtCreate(date);
+        articleForm.setGmtModify(date);
+
+        Article article = ArticleConvertor.convertArticleFormToArticle(articleForm);
+        articleDao.addArticle(article);
+
+        return ADD_ARTICLE_RESULT_JSP;
     }
 }
